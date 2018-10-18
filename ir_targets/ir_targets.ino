c64b93418852d7b478f61eb6526e989bd03123bd
Pranav Moktali <pranav.moktali@gmail.com>
2018-10-17 19:10:23 -0500

    added compare scoring

--- ir_targets/ir_targets.ino
+++ ir_targets/ir_targets.ino
@@ -1,59 +1,33 @@
 #include <IRremote.h>
 
 // Constants
-#define BASE_COUNT 2
+#define BASE_COUNT 1
 #define BLUE_LASER 0
 #define RED_LASER 1
 #define DEBUG_MODE true
 #define MIN_DELAY 3000
 #define DELAY_CEIL 4000
 
-//Spells from WAND A
-#define JAB_A1 0xFF629D
-#define JAB_A2 0x511DBB
-#define SWISH_A1 0x87C6D1F
-#define SWISH_A2 0x52A3D41F
-#define SWISH_A3 0xFF22DD
-#define SWIPE_A1 0xD7E84B1B
-#define SWIPE_A2 0xFF02FD
-
-//Spells from WAND B
-#define JAB_B1 0xFF906F
-#define JAB_B2 0xE5CFBD7F
-#define SWISH_B1 0xF92F9847
-#define SWISH_B2 0x1D320F8A
-#define SWISH_B3 0x6653181C
-#define SWISH_B4 0xF076C13B
-#define SWISH_B5 0xFFE01F
-#define SWISH_B6 0xF9EC0294
-#define SWIPE_B1 0xFFA857
-
-unsigned int jab_count_a = 0;
-unsigned int swish_count_a = 0;
-unsigned int swipe_count_a = 0;
-unsigned int unknown_action = 0;
-
-
-unsigned int jab_count_b = 0;
-unsigned int swish_count_b = 0;
-unsigned int swipe_count_b = 0;
 
 // Define all the devices as global variables
 IRrecv *irrecvs[BASE_COUNT];
 int ledPins[] = {2, 7}; // 2, 7, 4
-int receiverPins[] = {8, 12}; //8, 13, 12
+int receiverPins[] = {8}; //8, 13, 12
 
 // Define all time related variables
 unsigned long startMillis;
 unsigned long elapsedTime;
+unsigned long totalSpellCount = 0;
 
 
 // State variables
 int activeReceiver;
-bool laserHit[BASE_COUNT] = {false, false};
+bool laserHit[BASE_COUNT] = {false};
 
 // Store scores
 int scores[] = {0,0};
+long int values[] = {0,0};
+
 decode_results results[BASE_COUNT];
 
 // methods
@@ -79,6 +53,7 @@ void loop() {
 
   for (int i = 0; i < BASE_COUNT; i++) {
     if (!laserHit[i] && irrecvs[i]->decode(&results[i])) {
+     totalSpellCount++;
      recordHit(i);
 //     delay(1000);
     }
@@ -104,88 +79,28 @@ void recordHit(int index) {
 //  Serial.print(index);
 //  Serial.print("\n");
 
-  bool canPrint = true;
 
-  switch(results[index].value)
-  {
-    case 0xFFFFFFFF:
-      canPrint = false;
-      break;
-    case JAB_A1: 
-    case JAB_A2: 
-      jab_count_a ++;
-      break;
+  if (results[index].value != 0xFFFFFFFF) {
+    Serial.println(results[index].value, DEC);
 
-    case SWISH_A1: 
-    case SWISH_A2: 
-    case SWISH_A3:
-      swish_count_a ++;
-      break;
-
-    case SWIPE_A1:
-    case SWIPE_A2:
-      swipe_count_a ++;
-      break;
-
-    case JAB_B1:
-    case JAB_B2:
-      jab_count_b ++;
-      break;
-
-      case SWISH_B1:
-      case SWISH_B2:
-      case SWISH_B3: 
-      case SWISH_B4:
-      case SWISH_B5: 
-      case SWISH_B6:   
-      swish_count_b ++;
-  
-      break;
-          
-      case SWIPE_B1:
-      swipe_count_b ++;
-
-      break;
-      
-    default:
-    Serial.println(results[index].value, HEX);
-    unknown_action++;
-      break;
+    int wandCode = totalSpellCount%2;
+    values[wandCode] = results[index].value;
+    if (values[0] > values[1]) {
+        scores[0]++;
+      } else {
+        scores[1]++;
+    }
   }
 
-  if (canPrint) {
-  // A
-  Serial.print("jab_count a: ");
-  Serial.print(jab_count_a);   
-  Serial.print("\n");   
 
-  Serial.print("swish_count a: ");
-  Serial.print(swish_count_a);
-  Serial.print("\n");   
 
-  Serial.print("swipe_count a: ");
-  Serial.print(swipe_count_a);
-  Serial.print("\n");   
-  
-  // B
-  Serial.print("jab_count b: ");
-  Serial.print(jab_count_b);
-  Serial.print("\n"); 
-
-  Serial.print("swish_count b");
-  Serial.print(swish_count_b);
-  Serial.print("\n"); 
-
-  Serial.print("swipe_count b: ");
-  Serial.print(swipe_count_b);
-  Serial.print("\n");   
-
-  // unknown
-  Serial.print("unknown action: ");
-  Serial.print(unknown_action);
+  Serial.print("Scores WandA: ");
+  Serial.print(scores[0]);
+  Serial.print(", Scores WandB: ");
+  Serial.print(scores[1]);
   Serial.print("\n");
 
-  }
+  Serial.print("\n");
 
   laserHit[index] = true;
   startMillis = millis();
