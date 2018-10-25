779b6c1db7fbdf1d6282233af8999abc7df8fb0d
Pranav Moktali <pranav.moktali@gmail.com>
2018-10-25 18:07:13 -0500

    code cleanup

--- ir_targets/ir_targets.ino
+++ ir_targets/ir_targets.ino
@@ -1,12 +1,8 @@
 #include <IRremote.h>
 
 // Constants
-#define BASE_COUNT 2
-#define BLUE_LASER 0
-#define RED_LASER 1
+#define BASE_COUNT 1
 #define DEBUG_MODE true
-#define MIN_DELAY 3000
-#define DELAY_CEIL 4000
 
 //Spells from WAND A
 #define JAB_A1 0xFF629D
@@ -32,45 +28,27 @@
 #define SWIPE_B1 0xFFA857
 
 // Actions
-#define JAB 0
-#define SWIPE 1
-#define SWISH 2
-
-unsigned int jab_count_a = 0;
-unsigned int swish_count_a = 0;
-unsigned int swipe_count_a = 0;
-unsigned int unknown_action = 0;
-
-
-unsigned int jab_count_b = 0;
-unsigned int swish_count_b = 0;
-unsigned int swipe_count_b = 0;
+#define JAB 'a'
+#define SWIPE 'w'
+#define SWISH 'f'
 
 // Define all the devices as global variables
 IRrecv *irrecvs[BASE_COUNT];
-int ledPins[] = {2, 7}; // 2, 7, 4
-int receiverPins[] = {8, 12}; //8, 13, 12
-
-// Define all time related variables
-unsigned long startMillis;
-unsigned long elapsedTime;
-
+int ledPins[] = {2}; // 2, 7, 4
+int receiverPins[] = {8}; //8, 13, 12
 
 // State variables
-int activeReceiver;
-bool laserHit[BASE_COUNT] = {false, false};
+bool laserHit = false;
 
 // Store scores
-int scores[] = {0,0};
-decode_results results[BASE_COUNT];
+decode_results result;
 
 // methods
 void setup() {
   Serial.begin(9600);
   initSensors();
-//  initLeds();
+  //  initLeds();
 }
-
 
 void initSensors() {
   for (int i = 0; i < BASE_COUNT; i++) {
@@ -81,196 +59,111 @@ void initSensors() {
 
 void initLeds() {
   for (int i = 0; i < BASE_COUNT; i++)
-    pinMode(ledPins[i], OUTPUT);
+  pinMode(ledPins[i], OUTPUT);
 }
 
 void loop() {
-
   for (int i = 0; i < BASE_COUNT; i++) {
-    if (!laserHit[i] && irrecvs[i]->decode(&results[i])) {
-     recordHit(i);
-//     delay(1000);
+    if (!laserHit && irrecvs[i]->decode(&result)) {
+      recordHit(i);
     }
-    if (laserHit[i]) {
-      laserHit[i] = false;
+
+    if (laserHit) {
+      laserHit = false;
     }
   }
-  
-}
-
-
-int pumpkin_action() {
-  return (rand() % 3);
-}
-int interval() {
-  // generate random intervel between 500ms to 2500ms
-    return (rand() % DELAY_CEIL) + MIN_DELAY;
-}
-
-bool isdoneSleeping() {
-  elapsedTime = millis() - startMillis;
-  return elapsedTime > interval();
 }
 
 void recordHit(int index) {
-//  Serial.print("this is from sensor: ");
-//  Serial.print(index);
-//  Serial.print("\n");
-
   bool canPrint = true;
-  int person_action;
-  int pumpkin_action = JAB; //FIXME: pumpkin_action();
-  switch(results[index].value)
-  {
-    case 0xFFFFFFFF:
-      canPrint = false;
-      break;
-    case JAB_A1: 
-    case JAB_A2: 
-      jab_count_a ++;
-      person_action = JAB;
-      break;
+  char pSpell = detectSpell(index);
 
-    case SWISH_A1: 
-    case SWISH_A2:
-    case SWISH_A3:
-    case SWISH_A4:
-    case SWISH_A5:
-      swish_count_a ++;
-      person_action = SWISH;
-      break;
-
-    case SWIPE_A1:
-    case SWIPE_A2:
-    case SWIPE_A3:
-      swipe_count_a ++;
-      person_action = SWIPE;
-      break;
-
-    case JAB_B1:
-    case JAB_B2:
-      jab_count_b ++;
-      person_action = JAB;
-
-      break;
-
-      case SWISH_B1:
-      case SWISH_B2:
-      case SWISH_B3: 
-      case SWISH_B4:
-      case SWISH_B5: 
-      case SWISH_B6:   
-      swish_count_b ++;
-      person_action = SWISH;
- 
-      break;
-          
-      case SWIPE_B1:
-      swipe_count_b ++;
-      person_action = SWIPE;
-
-      break;
-      
-    default:
-    Serial.println(results[index].value, HEX);
-    unknown_action++;
-      break;
-  }
-
-  if (canPrint) {
-  // A
-//  Serial.print("jab_count a: ");
-//  Serial.print(jab_count_a);   
-//  Serial.print("\n");   
-//
-//  Serial.print("swish_count a: ");
-//  Serial.print(swish_count_a);
-//  Serial.print("\n");   
-//
-//  Serial.print("swipe_count a: ");
-//  Serial.print(swipe_count_a);
-//  Serial.print("\n");   
-//  
-//  // B
-//  Serial.print("jab_count b: ");
-//  Serial.print(jab_count_b);
-//  Serial.print("\n"); 
-//
-//  Serial.print("swish_count b: ");
-//  Serial.print(swish_count_b);
-//  Serial.print("\n"); 
-//
-//  Serial.print("swipe_count b: ");
-//  Serial.print(swipe_count_b);
-//  Serial.print("\n");   
-//
-//  // unknown
-//  Serial.print("unknown action: ");
-//  Serial.print(unknown_action);
-//  Serial.print("\n");
-//  Serial.print("\n");
-
-  Serial.print("pumpkin_action: ");
-
-  switch(pumpkin_action) {
-    case JAB:
-      Serial.print("jab");
-      break;
-     case SWISH:
-      Serial.print("swish");
-      break;
-     case SWIPE:
-      Serial.print("swipe");
-      break;
-  }
-  
-  Serial.print("\n");
-  Serial.print("your action: ");
-
-
-  switch(person_action) {
-    case JAB:
-      Serial.print("jab");
-      break;
-     case SWISH:
-      Serial.print("swish");
-      break;
-     case SWIPE:
-      Serial.print("swipe");
-      break;
-  }
-
-  Serial.print("\n");
-
-  Serial.print("winner: ");
-  if(pumpkin_action == JAB){
-    if (person_action == JAB) {
-      Serial.print("tie");
-    } else if (person_action == SWIPE) {
-      Serial.print("you!");
-    } else {
-      Serial.print("pumpkin!");
-    }
-    
-  }
- }
-
- Serial.print("\n");
-
-   
-  laserHit[index] = true;
-  startMillis = millis();
+  writeSpellToSerial(pSpell);
+  printSpell(pSpell);
   resetReceiver(index);
 }
 
 
-// need this? 
-
-void turnOffReceiver(int index) {
-  digitalWrite(ledPins[index], LOW);
+void resetReceiver(int index) {
+  laserHit = true;
+  irrecvs[index]->resume();
 }
 
-void resetReceiver(int index) {
-  irrecvs[index]->resume();
+void writeSpellToSerial(char spell) {
+  Serial.write(spell);
+}
+
+char detectSpell(int index) {
+  char pSpell;
+  switch(result.value)
+  {
+    case 0xFFFFFFFF:
+    break;
+
+    case JAB_A1:
+    case JAB_A2:
+    pSpell = JAB;
+    break;
+
+    case SWISH_A1:
+    case SWISH_A2:
+    case SWISH_A3:
+    case SWISH_A4:
+    case SWISH_A5:
+    pSpell = SWISH;
+    break;
+
+    case SWIPE_A1:
+    case SWIPE_A2:
+    case SWIPE_A3:
+    pSpell = SWIPE;
+    break;
+
+    case JAB_B1:
+    case JAB_B2:
+    pSpell = JAB;
+    break;
+
+    case SWISH_B1:
+    case SWISH_B2:
+    case SWISH_B3:
+    case SWISH_B4:
+    case SWISH_B5:
+    case SWISH_B6:
+    pSpell = SWISH;
+    break;
+
+    case SWIPE_B1:
+    pSpell = SWIPE;
+    break;
+
+    default:
+
+    break;
+  }
+
+  return pSpell;
+}
+
+void printSpell(char pSpell) {
+  if (DEBUG_MODE) {
+    Serial.println(result.value, HEX);
+    Serial.print("\n");
+    Serial.print("your action: ");
+
+    switch(pSpell) {
+      case JAB:
+      Serial.print("jab");
+      break;
+      case SWISH:
+      Serial.print("swish");
+      break;
+      case SWIPE:
+      Serial.print("swipe");
+      break;
+    }
+
+    Serial.print("\n");
+  }
 }
 
