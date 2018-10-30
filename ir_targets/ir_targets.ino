3c34c79af62231e4bf487ca8d049cc514b8e8357
Pranav Moktali <pranav.moktali@gmail.com>
2018-10-30 11:34:04 -0500

    adds more spells

--- ir_targets/ir_targets.ino
+++ ir_targets/ir_targets.ino
@@ -4,33 +4,49 @@
 #define BASE_COUNT 1
 #define DEBUG_MODE true
 
-//Spells from WAND A
+// Spell: JAB
 #define JAB_A1 0xFF629D
 #define JAB_A2 0x511DBB
+#define JAB_B1 0xFF906F
+#define JAB_B2 0xE5CFBD7F
+#define JAB_A3 0x2EC19612
+
+// Spell SWISH
 #define SWISH_A1 0x87C6D1F
 #define SWISH_A2 0x52A3D41F
 #define SWISH_A3 0xFF22DD
 #define SWISH_A4 0xBCD2259E
 #define SWISH_A5 0xDB37307D
-#define SWIPE_A1 0xD7E84B1B
-#define SWIPE_A2 0xFF02FD
-#define SWIPE_A3 0x5CDD8FBD
-
-//Spells from WAND B
-#define JAB_B1 0xFF906F
-#define JAB_B2 0xE5CFBD7F
+#define SWISH_6 0xF9EC0295
 #define SWISH_B1 0xF92F9847
 #define SWISH_B2 0x1D320F8A
 #define SWISH_B3 0x6653181C
 #define SWISH_B4 0xF076C13B
 #define SWISH_B5 0xFFE01F
 #define SWISH_B6 0xF9EC0294
+
+//Spell: SWIPE
+#define SWIPE_A1 0xD7E84B1B
+#define SWIPE_A2 0xFF02FD
+#define SWIPE_A3 0x5CDD8FBD
+#define SWIPE_A4 0xA3C8EDDB
 #define SWIPE_B1 0xFFA857
+#define SWIPE_5 0x3F15A646
+#define SWIPE_6 0x1EF51DF5 
+#define SWIPE_7 0x98473814
+#define SWIPE_8 0xC845A620
+#define SWIPE_9 0x8D828EEA
+#define SWIPE_10 0x87FFECB2
+#define SWIPE_11 0x25AE7EE0
+#define SWIPE_12 0x1EF51DF6
+#define SWIPE_13 0x4AB0F7B7
+
 
 // Actions
 #define JAB 'a'
 #define SWIPE 'w'
 #define SWISH 'f'
+#define INVALID 'n'
 
 // Define all the devices as global variables
 IRrecv *irrecvs[BASE_COUNT];
@@ -78,8 +94,14 @@ void recordHit(int index) {
   bool canPrint = true;
   char pSpell = detectSpell(index);
 
-  writeSpellToSerial(pSpell);
-  printSpell(pSpell);
+  if (pSpell != INVALID) {
+    if (DEBUG_MODE) {
+      printSpell(pSpell);
+    } else {
+      writeSpellToSerial(pSpell);
+    }
+  }
+
   resetReceiver(index);
 }
 
@@ -98,10 +120,14 @@ char detectSpell(int index) {
   switch(result.value)
   {
     case 0xFFFFFFFF:
+    pSpell = INVALID;
     break;
 
     case JAB_A1:
     case JAB_A2:
+    case JAB_B1:
+    case JAB_B2:
+    case JAB_A3:
     pSpell = JAB;
     break;
 
@@ -110,60 +136,56 @@ char detectSpell(int index) {
     case SWISH_A3:
     case SWISH_A4:
     case SWISH_A5:
-    pSpell = SWISH;
-    break;
-
-    case SWIPE_A1:
-    case SWIPE_A2:
-    case SWIPE_A3:
-    pSpell = SWIPE;
-    break;
-
-    case JAB_B1:
-    case JAB_B2:
-    pSpell = JAB;
-    break;
-
     case SWISH_B1:
     case SWISH_B2:
     case SWISH_B3:
     case SWISH_B4:
     case SWISH_B5:
     case SWISH_B6:
+    case SWISH_6:
     pSpell = SWISH;
     break;
 
+    case SWIPE_A1:
+    case SWIPE_A2:
+    case SWIPE_A3:
+    case SWIPE_A4:
     case SWIPE_B1:
+    case SWIPE_5:
+    case SWIPE_6:
+    case SWIPE_7:
+    case SWIPE_8:
+    case SWIPE_9:
+    case SWIPE_10:
+    case SWIPE_11:
+    case SWIPE_12:
     pSpell = SWIPE;
     break;
 
-    default:
-
-    break;
   }
 
   return pSpell;
 }
 
 void printSpell(char pSpell) {
-  if (DEBUG_MODE) {
-    Serial.println(result.value, HEX);
-    Serial.print("\n");
-    Serial.print("your action: ");
+  Serial.print("\n");
+  Serial.print("your action: ");
 
-    switch(pSpell) {
-      case JAB:
-      Serial.print("jab");
-      break;
-      case SWISH:
-      Serial.print("swish");
-      break;
-      case SWIPE:
-      Serial.print("swipe");
-      break;
-    }
+  switch(pSpell) {
+    case JAB:
+    Serial.print("jab");
+    break;
+    case SWISH:
+    Serial.print("swish");
+    break;
+    case SWIPE:
+    Serial.print("swipe");
+    break;
 
-    Serial.print("\n");
+    default:
+    Serial.print(result.value, HEX);
   }
+
+  Serial.print("\n");
 }
 
